ea21df8969dc43f05bc9b1056893fb501fde7b99
michalsn <michal@sniatala.pl>
2025-06-20 21:23:46 +0200

    cleanup

--- workflow/index.php
+++ workflow/index.php
@@ -6,13 +6,9 @@ use NeuronAI\Workflow\WorkflowState;
 use Weather\WeatherWorkflow;
 
 $state = new WorkflowState();
-$state->set('user_input', 'I live in Sydney, Australia.');
+$state->set('user_input', 'I live in Poznan, Poland.');
 
 $workflow = new WeatherWorkflow();
 $state = $workflow->run($state);
 
 echo $state->get('recommendation');
-echo PHP_EOL;
-echo $state->get('lat') . PHP_EOL;
-echo $state->get('lon') . PHP_EOL;
-echo $state->get('weather_info') . PHP_EOL;
